e21622749854ea027a5efd132285d6af48a67cd7
Vu A Nguyen <31049997+vunguyen1804@users.noreply.github.com>
2021-08-04 21:34:43 -0700

    Update password-generator.js

--- password-generator.js
+++ password-generator.js
@@ -1,20 +1,20 @@
 
 /*
-*	sha_512 (funct): sub-routine returns SHA-512 hash of string s
-* 	hmac_sha512 (funct): sub-routine returns HMAC hash of string s using key k
-*   to_base (funct): sub-routine returns hash h to string base b 
-*	mk (string): master key 
-*   ks (array): array of string keys
-* 	l (number): disired length of the output
-*   b (number): desired output base (58/64/85) 
+* sha_512 (funct): sub-routine returns SHA-512 hash of string s
+* hmac_sha512 (funct): sub-routine returns HMAC hash of string s using key k
+* to_base (funct): sub-routine returns hash h to string base b 
+* mk (string): master key 
+* ks (array): array of string keys
+* l (number): disired length of the output
+* b (number): desired output base (58/64/85) 
 */
 
 const sha_512 = s => {}					
 const hmac_sha512 = (s, k) => {}
 const to_base = (h, b) => {}	
 
-const generate_hash = (mk, l, ks) => {
-	let hash = hmac_sha512(l, mk)
+const generate_hash = (mk, ks) => {
+	let hash = sha_512(mk)
 
 	ks.forEach(k => {
 		hash = hmac_sha512(hash, k)
@@ -25,7 +25,7 @@ const generate_hash = (mk, l, ks) => {
 
 const generate_password = (mk, ks, l, b) =>{
 	let password = "";
-	let hash = generate_hash(mk, l.toString(), ks);
+	let hash = generate_hash(mk, ks);
 
 	while( password.length < l ){
 		password += to_base(hash, b);
